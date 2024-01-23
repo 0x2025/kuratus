d39bfcdcc918da8e11ad44502a5edfed01bf0452
Arthur <arthur@192.168.1.2>
2024-01-23 09:33:30 +0700

    init

---
+++ desktop/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    // Register a 'CommandOrControl+X' shortcut listener.
+    const ret = globalShortcut.register('CommandOrControl+X', () => {
+      console.log('CommCoaCondOrControl+X is pressed')
+      
+    })
+  
+    if (!ret) {
+      console.log('registration failed')
+    }
+  
+    // Check whether a shortcut is registered.
+    console.log(globalShortcut.isRegistered('CommandOrControl+X'))
+    createWindow()
+  })
+  
+  app.on('will-quit', () => {
+    // Unregister a shortcut.
+    globalShortcut.unregister('CommandOrControl+X')
+  
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
+  })
