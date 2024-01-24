de7d2da6f6ff57eb1146d40b718c83f5d6ac18d0
Sang <sang.cungoc@gmail.com>
2024-01-24 21:36:28 +0700

    feat: add ui for form
    To start generating css, run the command: npx tailwindcss -i ./src/css/quick-add.css -o ./src/css/quick-add.out.css --watch

--- electron/main.js
+++ electron/main.js
@@ -9,29 +9,29 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  win.loadFile('index.html')
+  win.loadFile('./src/quick-add.html')
 }
 
 app.whenReady().then(() => {
-    // Register a 'CommandOrControl+X' shortcut listener.
-    const ret = globalShortcut.register('CommandOrControl+X', () => {
-      console.log('CommCoaCondOrControl+X is pressed')
-      
-    })
-  
-    if (!ret) {
-      console.log('registration failed')
-    }
-  
-    // Check whether a shortcut is registered.
-    console.log(globalShortcut.isRegistered('CommandOrControl+X'))
-    createWindow()
-  })
-  
-  app.on('will-quit', () => {
-    // Unregister a shortcut.
-    globalShortcut.unregister('CommandOrControl+X')
-  
-    // Unregister all shortcuts.
-    globalShortcut.unregisterAll()
-  })
+  // // Register a 'CommandOrControl+X' shortcut listener.
+  // const ret = globalShortcut.register('CommandOrControl+X', () => {
+  //   console.log('CommCoaCondOrControl+X is pressed')
+
+  // })
+
+  // if (!ret) {
+  //   console.log('registration failed')
+  // }
+
+  // // Check whether a shortcut is registered.
+  // console.log(globalShortcut.isRegistered('CommandOrControl+X'))
+  createWindow()
+})
+
+app.on('will-quit', () => {
+  // Unregister a shortcut.
+  globalShortcut.unregister('CommandOrControl+X')
+
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll()
+})
