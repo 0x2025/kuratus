a35c54850f9f7aaeb7775f4049aa528d9c5fc4fa
Sang <sang.cungoc@gmail.com>
2024-01-25 11:54:25 +0700

    feat: implement hotkey and quit menu context

--- electron/main.js
+++ electron/main.js
@@ -1,10 +1,16 @@
 const { app, BrowserWindow, globalShortcut } = require('electron')
-const path = require('node:path')
+const { Tray, Menu, nativeImage } = require('electron')
+const appState = require('./src/appState')
 
+const path = require('node:path')
+let win;
 const createWindow = () => {
-  const win = new BrowserWindow({
+  if (win && !win.isDestroyed()) {
+    return
+  }
+  win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 500,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -13,24 +19,48 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
-  // // Register a 'CommandOrControl+X' shortcut listener.
-  // const ret = globalShortcut.register('CommandOrControl+X', () => {
-  //   console.log('CommCoaCondOrControl+X is pressed')
+  // Register a 'CommandOrControl+X' shortcut listener.
+  const ret = globalShortcut.register('CommandOrControl+K', () => {
+    console.log('CommandOrControl+K is pressed')
+    createWindow()
+  })
 
-  // })
+  if (!ret) {
+    console.log('registration failed')
+    createWindow()
+    return;
+  }
 
-  // if (!ret) {
-  //   console.log('registration failed')
-  // }
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('CommandOrControl+K'))
+  const icon = nativeImage.createFromPath(__dirname + '/assets/kuratus.png')
+  tray = new Tray(icon)
 
-  // // Check whether a shortcut is registered.
-  // console.log(globalShortcut.isRegistered('CommandOrControl+X'))
-  createWindow()
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Open', type: 'normal' },
+    { label: 'Options', type: 'normal' },
+    { label: '', type: 'separator' },
+    {
+      label: 'Quit', type: 'normal', click: () => {
+        appState.userQuit();
+        app.quit()
+      }
+    }
+  ])
+
+  tray.setContextMenu(contextMenu)
+  tray.setToolTip('Command+K to curate your item.')
+  tray.setTitle('') // use icon only
 })
 
-app.on('will-quit', () => {
+app.on('will-quit', (e) => {
+  if (!appState.forceQuit()) {
+    e.preventDefault();
+    return;
+  }
+
   // Unregister a shortcut.
-  globalShortcut.unregister('CommandOrControl+X')
+  globalShortcut.unregister('CommandOrControl+K')
 
   // Unregister all shortcuts.
   globalShortcut.unregisterAll()
