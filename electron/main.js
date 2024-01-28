3d1ba98c552a6638837b1686b7b2c94674d54eee
khangNguyen18 <minhkhang072003@gmail.com>
2024-01-28 23:07:10 +0700

    add icon for Kuratus app

--- electron/main.js
+++ electron/main.js
@@ -11,6 +11,7 @@ const createWindow = () => {
   win = new BrowserWindow({
     width: 800,
     height: 500,
+    icon: 'assets/kuratus@2x.png',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
