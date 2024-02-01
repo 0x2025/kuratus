587c38d9828310fa95ca6bf552776500f7e72bb5
Sang <sang.cungoc@gmail.com>
2024-02-01 17:09:22 +0700

    proxying blog

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  rewrites: () => {
-    return [
-        {
-            source: "/blog",
-            destination: "https://blog.kuratus.com/blog",
-        }
-    ];
-}
+    reactStrictMode: true,
+    rewrites: () => {
+        return [
+            {
+                source: "/blog",
+                destination: "https://blog.kuratus.com",
+            },
+            {
+                source: '/blog/:path*',
+                destination: 'https://blog.kuratus.com/:path*',
+            }
+        ];
+    }
 };
 
 export default nextConfig;
