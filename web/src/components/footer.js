3bd607d56304bc89f102fb9820029b355b7b2311
Sang <sang.cungoc@gmail.com>
2024-02-09 11:37:35 +0700

    use Link

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <div class="fixed bottom-0 left-0 flex h-28 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black">
@@ -7,8 +9,8 @@ export default function Footer() {
                         <div class="mx-auto max-w-2xl lg:max-w-5xl">
                             <div class="flex flex-col items-center justify-between gap-6 sm:flex-row">
                                 <div class="flex flex-wrap justify-center gap-x-6 gap-y-1 text-sm font-medium text-zinc-800 dark:text-zinc-200">
-                                    <a class="transition hover:text-lime-500 dark:hover:text-lime-400" href="/signup">Sign Up</a>
-                                    <a class="transition hover:text-lime-500 dark:hover:text-lime-400" href="/signin">Sign In</a>
+                                    <Link className="transition hover:text-lime-500 dark:hover:text-lime-400" href="/signup">Sign Up</Link>
+                                    <Link className="transition hover:text-lime-500 dark:hover:text-lime-400" href="/signin">Sign In</Link>
                                 </div>
                                 <p class="text-sm text-zinc-400 dark:text-zinc-500">©Kuratus. All rights reserved.</p>
                             </div>
