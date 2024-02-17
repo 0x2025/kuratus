823947e9094a7a358ebb639f953f6e45e858fe09
Sang <sang.cungoc@gmail.com>
2024-02-17 19:13:44 +0700

    build

--- blog/.eleventy.js
+++ blog/.eleventy.js
@@ -251,7 +251,7 @@ module.exports = function (eleventyConfig) {
       includes: "_includes",
       data: "_data",
       // Warning hardcoded throughout repo. Find and replace is your friend :)
-      output: "_site",
+      output: "_site"
     },
   };
 };
